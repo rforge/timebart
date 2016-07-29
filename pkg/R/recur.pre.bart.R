943c1dc508495341e9b43806f6663d70fff6a1b6
rsparapa <rsparapa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-29 02:21:22 +0000

    fix time zero bug and add bladder example
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timebart@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/recur.pre.bart.R
+++ pkg/R/recur.pre.bart.R
@@ -9,14 +9,14 @@
 recur.pre.bart <- function(
                       times,
                       ## matrix of recur times
-                      
+
                       delta,
                       ## matrix of indicators: 0=no event, 1=event
-    
+
                       x.train=NULL,
                       ## matrix of covariate regressors
                       ## can be NULL, i.e. KM analog
-                      
+
                       x.test=NULL
                       ## matrix of covariate regressors at X.test settings
                       ## does nothing for now since there is no obvious basis for v(t) and N(t-)
@@ -31,20 +31,22 @@ recur.pre.bart <- function(
     times <- cbind(times, apply(times, 1, max))
 
     dimnames(times)[[2]][J+1] <- 'stop'
-    
+
     events <- unique(sort(times))
     ## time grid of events including censoring times
+
+    if(events[1]==0) events <- events[-1]
 
     K <- length(events)
 
     y.train <- integer(N) ## y.train is at least N long
-            
+
     k <- 1
-        
+
     for(i in 1:N) for(j in 1:K) if(events[j] <= times[i, J+1]) {
         y.train[k] <- 0
 
-        for(h in 1:J) if(y.train[k]==0 & events[j]==times[i, h] & delta[i, h]==1) 
+        for(h in 1:J) if(y.train[k]==0 & events[j]==times[i, h] & delta[i, h]==1)
             y.train[k] <- 1
 
         k <- k+1
@@ -55,24 +57,24 @@ recur.pre.bart <- function(
     if(length(x.train)==0) {
         p <- 0
         n <- 1
-        
+
         X.train <- matrix(nrow=m, ncol=3)
 
         dimnames(X.train)[[2]] <- c('t', 'v', 'N')
     } else {
         p <- ncol(x.train)
-        
+
         if(length(x.test)>0) n <- nrow(x.test)
-        
+
         ##print(list(m, n, p))
-        
+
         X.train <- matrix(nrow=m, ncol=p+3)
 
         if(length(dimnames(x.train)[[2]])>0)
             dimnames(X.train)[[2]] <- c('t', 'v', 'N', dimnames(x.train)[[2]])
         else dimnames(X.train)[[2]] <- c('t', 'v', 'N', paste0('x', 1:p))
     }
-    
+
     k <- 1
 
     for(i in 1:N) {
@@ -90,7 +92,7 @@ recur.pre.bart <- function(
             if(p>0) X.train[k, 4:(3+p)] <- x.train[i, ]
 
             k <- k+1
-        }        
+        }
     }
 
 ## automated X.test creation is not feasible since there is no obvious basis for v(t) and N(t-)
@@ -104,7 +106,7 @@ recur.pre.bart <- function(
     ##     }
     ## }
     ## else X.test <- matrix(0.0, 0L, 0L)
-    
+
     return(list(y.train=y.train, X.train=X.train, X.test=matrix(0.0, 0L, 0L), times=events, K=K))
                 ##X.test=data.matrix(X.test), times=events, K=K))
 }

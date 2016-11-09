15cbd53e76a241df1b36ce43e58366af3ea2707d
rsparapa <rsparapa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-11-09 03:10:33 +0000

    defer to surv.pre.bart
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timebart@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/crisk.pre.bart.R
+++ pkg/R/crisk.pre.bart.R
@@ -24,62 +24,15 @@ crisk.pre.bart <- function(
     ## can be extended later
     ## most likely via the alternative counting process notation
 
-    N <- length(times)
-
-    events <- unique(sort(times))
-    ## time grid of events including censoring times
-
-    K <- length(events)
-
-    y.train1 <- integer(N) ## y.train for cause 1
-    y.train2 <- integer(N) ## y.train for cause 2
-            
-    k <- 1
+    stopifnot(all(unique(sort(delta))==0:2))
         
-    for(i in 1:N) for(j in 1:K) if(events[j] <= times[i]) {
-        y.train1[k] <- (delta[i]==1)*(times[i] == events[j])
-        y.train2[k] <- (delta[i]==2)*(times[i] == events[j])
+    pre <- surv.pre.bart(times=times, 1*(delta==1), x.train=x.train, x.test=x.test)
 
-        k <- k+1
-    }
-
-    m <- length(y.train1)
-
-    if(length(x.train)==0) {
-        p <- 0
-        n <- 1
-        
-        X.train <- matrix(nrow=m, ncol=1, dimnames=list(NULL, 't'))
-    } else {
-        p <- ncol(x.train)
-        
-        if(length(x.test)>0) n <- nrow(x.test)
-        
-        X.train <- matrix(nrow=m, ncol=p+1)
-
-        if(length(dimnames(x.train)[[2]])>0)
-            dimnames(X.train)[[2]] <- c('t', dimnames(x.train)[[2]])
-        else dimnames(X.train)[[2]] <- c('t', paste0('x', 1:p))
-    }
+    pre$cond <- which(pre$y.train==0)
     
-    k <- 1
+    pre2 <- surv.pre.bart(times=times, 1*(delta==2))
     
-    for(i in 1:N) for(j in 1:K) if(events[j] <= times[i]) {
-        if(p==0) X.train[k, ] <- c(events[j])
-        else X.train[k, ] <- c(events[j], x.train[i, ])
+    pre$y.train2 <- pre2$y.train
 
-        k <- k+1
-    }
-
-    if(p==0 | length(x.test)>0) {
-        X.test <- matrix(nrow=K*n, ncol=p+1, dimnames=dimnames(X.train))
-
-        for(i in 1:n) for(j in 1:K) {
-            if(p==0) X.test[j, ] <- c(events[j])
-            else X.test[(i-1)*K+j, ] <- c(events[j], x.test[i, ])
-        }
-    }
-    else X.test <- matrix(nrow=0, ncol=0)*0
-    
-    return(list(y.train1=y.train1, y.train2=y.train2, X.train=X.train, X.test=X.test, times=events, K=K))
+    return(pre)
 }

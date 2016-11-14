8c63b6054fcfe54b71a4f3b8976bd7f2d00ac79f
rsparapa <rsparapa@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-11-14 14:43:28 +0000

    due to the 2Gi limit in sendMaster loop over reduced ndpost
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/timebart@58 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mc.surv.bart.R
+++ pkg/R/mc.surv.bart.R
@@ -33,19 +33,16 @@ mc.surv.bart <- function(
         if(length(binaryOffset)==0) binaryOffset <- pre$binaryOffset
     }
     else if(length(binaryOffset)==0) binaryOffset <- 0
+
+    H <- 1
     Mx <- 2^31-1
-    Nx <- nrow(x.test)
-    if(Nx>Mx%/%ndpost) {
-        ndpost <- Mx %/% Nx
-        message('nrow(x.test)*ndpost>2Gi: due to the 2Gi limit in sendMaster,\n',
-                '(unless this limit was increased): reducing ndpost to ', ndpost)
-    }
+    Nx <- max(nrow(x.train), nrow(x.test))
     
-    Nx <- nrow(x.train)
     if(Nx>Mx%/%ndpost) {
-        ndpost <- Mx %/% Nx
-        message('nrow(x.test)*ndpost>2Gi: due to the 2Gi limit in sendMaster,\n',
-                '(unless this limit was increased): reducing ndpost to ', ndpost)
+        H <- ceiling(ndpost / (Mx %/% Nx))
+        ndpost <- ndpost %/% H
+        ##nrow*ndpost>2Gi: due to the 2Gi limit in sendMaster
+        ##(unless this limit was increased): reducing ndpost 
     }
     
     mc.cores.detected <- detectCores()
@@ -61,7 +58,10 @@ mc.surv.bart <- function(
 
     while(mc.ndpost*mc.cores<ndpost) mc.ndpost <- mc.ndpost+keepevery
 
-    for(i in 1:mc.cores) {
+    post.list <- list()
+        
+    for(h in 1:H) {
+        for(i in 1:mc.cores) {
         parallel::mcparallel({psnice(value=nice);
               surv.bart(x.train=x.train, y.train=y.train, x.test=x.test,
                         keepcall=keepcall, k=k,
@@ -74,28 +74,32 @@ mc.surv.bart <- function(
                         verbose=verbose)}, silent=(i!=1))
                                           ## to avoid duplication of output
                                           ## capture stdout from first posterior only
+        }
+
+        post.list[[h]] <- parallel::mccollect()
     }
 
-    post.list <- parallel::mccollect()
-
-    post <- post.list[[1]]
-
-    if(mc.cores==1) return(post)
+    if(H==1 & mc.cores==1) return(post.list[[1]][[1]])
     else {
-        for(i in 2:mc.cores) {
-            if(keeptrainfits) post$yhat.train <- rbind(post$yhat.train, post.list[[i]]$yhat.train)
+        for(h in 1:H) for(i in mc.cores:1) {
+            if(h==1 & i==mc.cores) post <- post.list[[1]][[mc.cores]]
+            else {
+                if(keeptrainfits) post$yhat.train <- rbind(post$yhat.train, post.list[[h]][[i]]$yhat.train)
 
-            if(length(post$surv.train)>0)
-                post$surv.train <- rbind(post$surv.train, post.list[[i]]$surv.train)
+                if(length(post$surv.train)>0)
+                    post$surv.train <- rbind(post$surv.train, post.list[[h]][[i]]$surv.train)
 
-            if(length(post$yhat.test)>0)
-                post$yhat.test <- rbind(post$yhat.test, post.list[[i]]$yhat.test)
+                if(length(post$yhat.test)>0)
+                    post$yhat.test <- rbind(post$yhat.test, post.list[[h]][[i]]$yhat.test)
 
-            if(length(post$surv.test)>0)
-                post$surv.test <- rbind(post$surv.test, post.list[[i]]$surv.test)
+                if(length(post$surv.test)>0)
+                    post$surv.test <- rbind(post$surv.test, post.list[[h]][[i]]$surv.test)
 
-            post$varcount <- rbind(post$varcount, post.list[[i]]$varcount)
-        }
+                post$varcount <- rbind(post$varcount, post.list[[h]][[i]]$varcount)
+                      }                      
+
+            post.list[[h]][[i]] <- NULL
+            }
 
         if(length(post$yhat.train.mean)>0)
             post$yhat.train.mean <- apply(post$yhat.train, 2, mean)
